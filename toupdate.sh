899e17f17788efaa12108ff2194ed3b2aec7003b
Motong LI <ribokuto@gmail.com>
2019-11-19 01:00:46 +0800

    Build website

---
+++ toupdate.sh
@@ -0,0 +1,9 @@
+#!/user/bin/env bash
+
+# 名前に振り仮名をつける
+sed -i -e 's#<h2 itemprop="name">李 墨彤</h2>#<h2 itemprop="name"><ruby>李<rt>り</rt></ruby> <ruby>墨彤<rt>ぼくとう</rt></ruby></h2>#g' index.html
+git add .
+git commit -m "Build website"
+
+git push origin master
+# cd ..
